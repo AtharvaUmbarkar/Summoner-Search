8124c1425e9dffdd705d063042984c5abe8d28e2
Atharva Umbarkar <57295061+AtharvaUmbarkar@users.noreply.github.com>
2022-04-08 19:59:01 +0530

    Fix Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,15 +14,19 @@ const Navbar = () => {
 
   return (
     <>
-      <div className={`nav-menu ${menuOpen ? 'block w-full' : 'hidden w-0'} backdrop-blur-sm bg-opacity-70 bg-clrBackground-1`}>
-        <div className='h-full absolute w-1/2 bg-clrBackground-1 bg-opacity-100'>
-          <ul className='text-clrContent-0 mt-14 mx-4 space-y-2 text-sm flex flex-col justify-evenly items-start'>
-            <Link to="/home" onClick={toggleMenu} className='w-full  hover:text-clrContent-3'>Home</Link>
-            <Link to="/main_stats" onClick={toggleMenu} className='w-full  hover:text-clrContent-3'>Statistics</Link>
-            <Link to="/match_history" onClick={toggleMenu} className='w-full  hover:text-clrContent-3'>Match History</Link>
-            <Link to="/about" onClick={toggleMenu} className='w-full  hover:text-clrContent-3'>About</Link>
-          </ul>
+      <div className={`nav-menu ${menuOpen ? 'w-full' : 'w-0'} h-full absolute backdrop-blur-sm bg-opacity-70 bg-clrBackground-1`}>
+        <div className='flex flex-row h-full w-full'>
+          <div className='h-full w-1/2 bg-clrBackground-1 bg-opacity-100'>
+            <ul className={`text-clrContent-0 mt-14 mx-4 space-y-2 text-sm flex flex-col justify-evenly items-start ${menuOpen ? 'visible opacity-100' : 'invisible opacity-0'} nav-menu-list`}>
+              <Link to="/home" onClick={toggleMenu} className='w-full  hover:text-clrContent-3'>Home</Link>
+              <Link to="/main_stats" onClick={toggleMenu} className='w-full  hover:text-clrContent-3'>Statistics</Link>
+              <Link to="/match_history" onClick={toggleMenu} className='w-full  hover:text-clrContent-3'>Match History</Link>
+              <Link to="/about" onClick={toggleMenu} className='w-full  hover:text-clrContent-3'>About</Link>
+            </ul>
+          </div>
+          <div onClick={toggleMenu} className='h-full w-1/2'></div>
         </div>
+
       </div>
       <nav className='navbar nav-flex'>
         {!menuOpen ?
